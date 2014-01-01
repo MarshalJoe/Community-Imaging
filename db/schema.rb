e28d87fe93f3252f2586134b9d7a9c048d84730f
lynda wellhausen <lyndawellhausen@gmail.com>
2013-12-31 19:41:04 -0600

    allow f & l name to persist to user table via permitted params

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140101003040) do
+ActiveRecord::Schema.define(version: 20140101005119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140101003040) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "fname"
+    t.string   "lname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
