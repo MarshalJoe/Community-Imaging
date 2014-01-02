b2f56c4852c38330850ffcccb06f31c864447b23
lynda wellhausen <lyndawellhausen@gmail.com>
2014-01-01 21:51:34 -0600

    changed to screenings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Center::Application.routes.draw do
   devise_for :users
- root "registration#index"
- get '/services/', to: 'registration#services', as: 'services'
- get '/about/', to: 'registration#about', as: 'about'
- get '/contact/', to: 'registration#contact', as: 'contact'
- get '/patients/', to: 'registration#patients', as: 'patients'
+ root "screenings#index"
+ get '/services/', to: 'screenings#services', as: 'services'
+ get '/about/', to: 'screenings#about', as: 'about'
+ get '/contact/', to: 'screenings#contact', as: 'contact'
+ get '/patients/', to: 'screenings#patients', as: 'patients'
+
+ resources :screenings, except: :new
 end
