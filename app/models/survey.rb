e1ebb8d0364f9a66aecadd91944cea177641668b
lynda wellhausen <lyndawellhausen@gmail.com>
2014-01-11 17:30:35 -0600

    add rest of survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -8,21 +8,31 @@ class Survey < ActiveRecord::Base
                       [5, 'Excellent'],
                     ]
 
-
   validates :scheduling, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :receptionist_greeting, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :completing_forms, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :awaiting_procedure, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :technologist, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :explanation, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :patient_privcacy, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :cleanliness, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :overall_experience, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
+  validates :hear_about, inclusion: {in: SURVEY_OPTIONS.map(&:first)}
 
 
-  # calling this method will return the appropriate survey option text
+  # return the appropriate survey option text
   def display_field(field)
     val = send(field.to_sym)
     survey_options_hash[val] 
   end
 
-
+  # 
   def survey_options_hash
     @survey_options_hash ||= SURVEY_OPTIONS.inject({}) { |hash,pair| hash.merge(pair.first => pair.last) }
   end
 
-
-
 end
+
+
+
+
